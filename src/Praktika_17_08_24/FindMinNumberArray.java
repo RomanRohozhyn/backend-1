c24ad22bb90c07fe9c67aaca1a229632c4b998fc
AIT TR Student <Romano305@gmail.com>
2024-08-18 00:39:29 +0200

    find min number

--- src/Praktika_17_08_24/FindMinNumberArray.java
+++ src/Praktika_17_08_24/FindMinNumberArray.java
@@ -1,4 +1,22 @@
 package Praktika_17_08_24;
 
 public class FindMinNumberArray {
+
+    public static void main(String[] args) {
+
+        int[] ints = {38,6,93,75,23,46};
+
+        int minNumber = ints[0];
+
+        for (int i = 1;i < ints.length;i++)
+        {
+            if (ints[i] < minNumber)
+           {
+               minNumber = ints[i];
+               System.out.println(minNumber);
+
+           }
+
+        }
+    }
 }
