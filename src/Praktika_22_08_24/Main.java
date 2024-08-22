2c7a86946cf2797fa8a1dbabc6a3bffedd056d39
AIT TR Student <Romano305@gmail.com>
2024-08-22 21:16:48 +0200

    neue Figura aus zahlen

---
+++ src/Praktika_22_08_24/Main.java
@@ -0,0 +1,24 @@
+package Praktika_22_08_24;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int n = 10;
+
+        while (i < n)
+        {
+            int j = 0;
+            while (j < i)
+            {
+                System.out.print(i);
+                j++;
+            }
+            i++;
+            System.out.println();
+
+        }
+
+    }
+
+}
