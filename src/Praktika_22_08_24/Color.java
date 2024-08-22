fb5c21e3e882cda7797f3b2cddb6a5857a861909
AIT TR Student <Romano305@gmail.com>
2024-08-22 21:35:48 +0200

    fehler Color

---
+++ src/Praktika_22_08_24/Color.java
@@ -0,0 +1,13 @@
+package Praktika_22_08_24;
+
+public class Color {
+
+    public static void main(String[] args) {
+
+        Color red = new Color();
+
+        System.out.println(red);
+          colorRed = new java.awt.Color(0xFF560C0C, true);
+
+    }
+}
