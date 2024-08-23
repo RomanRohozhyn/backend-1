41cde450c4b30a9c3373219f406645ce280ec161
AIT TR Student <Romano305@gmail.com>
2024-08-23 23:21:03 +0200

    neu figure 2

--- src/NeuFigureAusZahlen/Main1.java
+++ src/NeuFigureAusZahlen/Main1.java
@@ -3,7 +3,21 @@ package NeuFigureAusZahlen;
 public class Main1 {
     public static void main(String[] args) {
 
+        int i = 1;
+        int n = 10;
+
+        while (i < n) {
+            int j = 0;
+
+            while (j < i) {
+                int a = 0;
+                System.out.print(a);
+                j++;
+            }
+            i++;
+            System.out.println();
 
 
+        }
     }
 }
