26ddf48bcd8c601302f7c51e00fc02f322cb57e1
AIT TR Student <Romano305@gmail.com>
2024-08-23 22:35:29 +0200

    neu figure

---
+++ src/NeuFigureAusZahlen/Main.java
@@ -0,0 +1,41 @@
+package NeuFigureAusZahlen;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        int a = 0;
+        int n = 10;
+
+        while (i < n)
+        {
+            int j = 0;
+             i = 10;
+            while (j < i)
+            {
+
+                System.out.print(a);
+                j++;
+
+            }
+
+            System.out.println();
+            System.out.print(a);
+            System.out.print(" ");
+            System.out.print(" ");
+            System.out.print(" ");
+            System.out.print(" ");
+            System.out.print(" ");
+            System.out.print(" ");
+            System.out.print(" ");
+            System.out.print(" ");
+            System.out.print(a);
+            i++;
+
+
+
+        }
+
+    }
+}
---
+++ src/Praktika_23_08_24/NeuFigure.java
@@ -0,0 +1,25 @@
+package Praktika_23_08_24;
+
+public class NeuFigure {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        int n = 10;
+
+        while (i < n)
+        {
+            int j = 0;
+
+            while (j <= i)
+            {
+                System.out.print(i);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+
+
+    }
+}
